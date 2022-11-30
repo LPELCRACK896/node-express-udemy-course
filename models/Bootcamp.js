29c129de3275aadcb3d1f5fbb02274a4e24483f5
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-11-29 22:38:57 -0600

    geocode middleware to build location on bootcamp scheema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-
+const geocoder = require('../utils/geocode')
 
 const BootCampScheema = new mongoose.Schema({
   name: {
@@ -103,6 +103,23 @@ const BootCampScheema = new mongoose.Schema({
 // Create Bootcamp slug from the name -> Using Mongoose Middleware
 BootCampScheema.pre('save', function(next){ //must be a regular function
   this.slug = slugify(this.name, { lower: true })
+  next()
+})
+//Geocode & Create location field
+BootCampScheema.pre('save', async function(next){
+  const loc = await geocoder.geocode(this.address)
+  this.location = {
+    type: 'Point', 
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  }
+  // Do not save addres in DB
+  this.address = undefined
   next()
 })
 module.exports = mongoose.model('Bootcamp', BootCampScheema)
