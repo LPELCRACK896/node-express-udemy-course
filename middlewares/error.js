3e24edd3427a3a814229d32118e352976e69f50f
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-11-29 20:54:01 -0600

    more mongoose error handlings

--- middlewares/error.js
+++ middlewares/error.js
@@ -2,15 +2,27 @@ const ErrorResponse = require("../utils/errorResponse")
 
 const errorHandler = (err, req, res, next) =>{
     // Log to console for dev
-    console.log(err.stack.red)
+    console.log(err)
 
     let error = {...err}
     error.message = err.message
 
-    let message
+    // Mongoose bad ObjecetID
     if(err.name === 'CastError')  {
-        message = `Resourse not found with id of ${err.value}`
+        const message = `Resourse not found with id of ${err.value}`
         error = new ErrorResponse(message, 404)
+    }
+    // Mongoose duplicate key
+    if(err.code === 11000){
+        const message = 'Duplicates field value entered'
+        error = new ErrorResponse(message, 400)
+    } 
+    // Mongoose validation error
+    if (err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message)
+        console.log(typeof(message))
+        console.log(message)
+        error = new ErrorResponse(message, 400)
     }
     res.status(error.statusCode || 500).json({
         succes: false, 
